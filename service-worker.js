92d145efe6ff4467ab87cb6b82e3934b3649fd22
ArdiTaqdis <kejajarwsb84@gmail.com>
2025-07-06 00:14:19 +0700

    pwaTEST

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,35 @@
-self.addEventListener("install", function(e) {
-  e.waitUntil(
-    caches.open("absensi-cache").then(function(cache) {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/style.css",
-        "/main.js",
-        "/manifest.json"
-      ]);
-    })
+const CACHE_NAME = "patroli-v1";
+const urlsToCache = [
+  "index.html",
+  "form.html",
+  "style.css",
+  "form.js",
+  "icon-192.png",
+  "icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  self.skipWaiting(); // ⬅️ langsung aktif
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch", function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
